6bfdf89dac6300d928827dc873ea29b7df32e691
Anastasiya Sharkouskaya <a.sharkovskaya@yandex.ru>
2021-09-11 23:39:36 +0200

    Add project files.

---
+++ Abc/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Abc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.learn_words_button = new System.Windows.Forms.Button();
+            this.check_botton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // learn_words_button
+            // 
+            this.learn_words_button.BackColor = System.Drawing.Color.YellowGreen;
+            this.learn_words_button.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.learn_words_button.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.learn_words_button.Location = new System.Drawing.Point(204, 381);
+            this.learn_words_button.Name = "learn_words_button";
+            this.learn_words_button.Size = new System.Drawing.Size(209, 92);
+            this.learn_words_button.TabIndex = 0;
+            this.learn_words_button.Text = "Учить слова";
+            this.learn_words_button.UseVisualStyleBackColor = false;
+            this.learn_words_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // check_botton
+            // 
+            this.check_botton.BackColor = System.Drawing.Color.SandyBrown;
+            this.check_botton.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.check_botton.Location = new System.Drawing.Point(481, 381);
+            this.check_botton.Name = "check_botton";
+            this.check_botton.Size = new System.Drawing.Size(209, 93);
+            this.check_botton.TabIndex = 1;
+            this.check_botton.Text = "Проверка знаний";
+            this.check_botton.UseVisualStyleBackColor = false;
+            this.check_botton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(293, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 565);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.check_botton);
+            this.Controls.Add(this.learn_words_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button learn_words_button;
+        private System.Windows.Forms.Button check_botton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
